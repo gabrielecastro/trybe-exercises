83f62f29dee7d2430747c4cb5ce179b484e96848
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-09-24 10:59:28 -0300

    exercício 1

---
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
@@ -0,0 +1,23 @@
+/* 01 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals . */
+
+function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+  testingScope(true);
+
+  /* 02 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+Copie o código abaixo. */
+
+
+  
