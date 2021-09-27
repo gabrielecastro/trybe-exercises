2ee2fbfe78866fe07e1e623156140a2c61770546
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-09-27 19:22:27 -0300

    Exercício 1

---
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte-2.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+/* 1 - Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+ */
+const newKey = (object, key, value) => {
+  object.key = value;
+}
+newKey(lesson2, 'turno', 'noite');
+console.log(lesson2);
