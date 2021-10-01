0b09945525fe4258d7ffe92238a3e4af87d4a1af
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-10-01 11:13:05 -0300

    cria testes para a função sum

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,4 @@
 const { it, expect } = require('@jest/globals');
-const { describe } = require('yargs');
 const sum = require('./sum');
 
 /* - A função sum(a, b) retorna a soma do parâmetro a com o b
@@ -8,16 +7,25 @@ Teste se o retorno de sum(0, 0) é 0
 Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5") */
 
-
+describe('The function sum', () => {
   it('testa se a funçao existe', () => {
     expect(typeof sum).toBe('function')
   });
+
   it('testa se retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
+
   it('testa se retorno de sum(0, 0) é 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
+
+  it('testa se a função lança um erro quando os parâmetros são 4 e "5"', () => {
+    expect(() => sum(4, '5')).toThrow()
+  })
+
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => sum(4, '5')).toThrowError(new Error('parameters must be numbers'));
+  });
+});
   
-
-
