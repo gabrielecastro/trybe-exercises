7bfb3216f2b491e16043d6a51ba04a3e130800dd
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-10-02 10:16:02 -0300

    cria testes para a função myRemove

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,7 +1,7 @@
 const { it, expect } = require('@jest/globals');
-const sum = require('./sum');
+const {sum, myRemove} = require('./sum');
 
-/* - A função sum(a, b) retorna a soma do parâmetro a com o b
+/* 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
 Teste se o retorno de sum(4, 5) é 9
 Teste se o retorno de sum(0, 0) é 0
 Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
@@ -28,4 +28,20 @@ describe('The function sum', () => {
     expect(() => sum(4, '5')).toThrowError(new Error('parameters must be numbers'));
   });
 });
-  
+
+  /* 2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+  Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+  Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+  Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado */
+
+  describe('The function myRemove', () => {
+    it('verifica se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+      expect(myRemove([1, 2, 3, 4], 3)).toEqual([ 1, 2, 4 ]);
+    });
+    it('verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+      expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([ 1, 2, 3, 4 ]);
+    });
+    it('verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+      expect(myRemove([1, 2, 3, 4], 5)).toEqual([ 1, 2, 3, 4 ]);
+    });
+});
