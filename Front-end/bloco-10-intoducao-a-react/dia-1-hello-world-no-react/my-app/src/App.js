f03d7d2e109d0a76b869d4aa8f0f311a1b3890cd
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-10-28 13:01:06 -0300

    adiciona footer

--- Front-end/bloco-10-intoducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
+++ Front-end/bloco-10-intoducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import React, {Component} from 'react';
 import Content from './Content';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <>
       <Header />
       <Content />
+      <Footer />
       </>
     )
   }
