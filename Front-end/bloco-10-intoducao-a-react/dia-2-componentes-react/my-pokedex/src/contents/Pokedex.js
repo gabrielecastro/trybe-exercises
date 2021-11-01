b9622a4f56559f106621cdfabf14b0d34c2cd6e9
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-11-01 10:50:22 -0300

    Cria componente Pokedex e renderiza ele no arquivo principal

---
+++ Front-end/bloco-10-intoducao-a-react/dia-2-componentes-react/my-pokedex/src/contents/Pokedex.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import pokemons from '../data';
+import Pokemon from './Pokemon';
+
+class Pokedex extends Component {
+  render() { 
+    return <div>
+      {pokemons.map((pokemon) => (
+        <Pokemon 
+          nome={ pokemon.name }
+          tipo={`Tipo: ${ pokemon.type }`}
+          peso= {`Peso: ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}
+          imagem={ pokemon.image }
+        />
+      ))}
+      
+    </div>;
+  }
+}
+ 
+export default Pokedex;
