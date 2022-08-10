746710447ce866bf36247c0b44b1bae837e6af14
Gabriele <gabrielerayane2001@gmail.com>
2022-08-10 11:21:00 -0300

    subindo arquivos

---
+++ Computer Science/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercise03.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from math import pi as PI
+
+
+class Figura_Geometrica(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimetro(self):
+        pass
+
+
+class Quadrado(Figura_Geometrica):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado * self.lado
+
+    def perimetro(self):
+        return 4 * self.lado
+
+
+class Retangulo(Figura_Geometrica):
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+    def perimetro(self):
+        return 2 * (self.base + self.altura)
+
+
+class Circulo(Figura_Geometrica):
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return PI * self.raio * self.raio
+
+    def perimetro(self):
+        return 2 * PI * self.raio
