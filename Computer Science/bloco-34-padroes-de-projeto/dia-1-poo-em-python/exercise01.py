877030cafec48a25c847cdde266214238abad212
Gabriele <gabrielerayane2001@gmail.com>
2022-08-10 11:36:33 -0300

    feat: exercícios do dia 34.1

--- Computer Science/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercise01.py
+++ Computer Science/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercise01.py
@@ -1,27 +1,27 @@
 class tv:
-    def __init__(self, tamanho):
-        self.__volume = 50
-        self.__canal = 1
-        self.__tamanho = tamanho
-        self.__ligada = False
-
-    def aumentar_volume(self):
-        if self.__volume <= 99:
-            self.__volume += 1
-
-    def diminuir_volume(self):
-        if self.__volume >= 0:
-            self.__volume -= 1
-
-    def modificar_canal(self, canal):
-        if canal > 1 or canal < 99:
-            self.__canal = canal
-
-        else:
-            raise ValueError("Canal indisponível!")
-
-    def ligar_desligar(self):
-        if self.__ligada == False:
-            self.__ligada = True
-        else:
-            self.__ligada = False
+   def __init__(self, tamanho):
+       self.__volume = 50
+       self.__canal = 1
+       self.__tamanho = tamanho
+       self.__ligada = False
+ 
+   def aumentar_volume(self):
+       if self.__volume <= 99:
+           self.__volume += 1
+ 
+   def diminuir_volume(self):
+       if self.__volume >= 0:
+           self.__volume -= 1
+ 
+   def modificar_canal(self, canal):
+       if canal > 1 or canal < 99:
+           self.__canal = canal
+ 
+       else:
+           raise ValueError("Canal indisponível!")
+ 
+   def ligar_desligar(self):
+       if self.__ligada == False:
+           self.__ligada = True
+       else:
+           self.__ligada = False
